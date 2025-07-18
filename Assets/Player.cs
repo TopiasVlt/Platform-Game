05e1809cdd7fe20c7ecfab7f564754d848862f10
rosehairedsheep <76109159+rosehairedsheep@users.noreply.github.com>
2025-07-18 14:00:33 +0300

    Eka pusku

---
+++ Assets/Player.cs
@@ -0,0 +1,84 @@
+//using System.Diagnostics;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private int count;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count" + count.ToString();
+        if (count >= 8)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        
+        Jump();
+        Debug.Log("toka");
+    }
+
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector3(horizontalInput * moveSpeed, rb.linearVelocity.y, 0);
+
+        // Vector2 movement = new(horizontalInput * moveSpeed, rb.angularVelocity.y);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("eka");
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+           
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+
+    void Jump()
+    
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, 0);
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        }
+    }
+
+    
+    
+}
